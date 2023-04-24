63406b8ab7083748bbabe28896596d8952b945c2
陈先程 <cxc@spacetech.com.cn>
2023-04-24 13:56:17 +0800

    提交模板

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,41 +1,25 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
+  <view class="content" @click="dome">
+    {{ title }}
   </view>
 </template>
 
 <script setup lang="ts">
 // import { ref } from 'vue'
-const title = ref('Hello')
+const title = ref('Hello22')
+
+function dome() {
+console.log(123);
+
+  uni.scanCode({
+    success: function (res) {
+      console.log('条码类型：' + res.scanType)
+      console.log('条码内容：' + res.result)
+      // that.result1 = res.result;
+    },
+    fail() {},
+  })
+}
 </script>
 
-<style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
-</style>
+<style lang="scss"></style>
